ad03fcaffb637c9f30e31341c26abb4d860478f2
YoujungSon <andn1026@gmail.com>
2022-11-29 16:55:43 +0900

    #1.3 Subscriptions

--- src/index.js
+++ src/index.js
@@ -15,11 +15,20 @@ const countModifier = (count = 0, action) => {
 };
 
 const countStore = createStore(countModifier);
+number.innerText = 0;
 
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'ADD' });
-countStore.dispatch({ type: 'MINUS' });
-console.log(countStore.getState());
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: 'ADD' });
+};
+const handleMinus = () => {
+  countStore.dispatch({ type: 'MINUS' });
+};
+
+add.addEventListener('click', handleAdd);
+minus.addEventListener('click', handleMinus);
